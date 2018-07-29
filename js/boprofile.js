a8c49ac933e56c6e84c1f405588989695908fa46
Sean <s10162177@connect.np.edu.sg>
2018-07-29 22:48:27 +0800

    Updated Table

--- js/boprofile.js
+++ js/boprofile.js
@@ -1,50 +1,4 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User Sign Up
-
-    window.location = 'index.html'
-
-    
-
-  } else {
-    // No user is signed in.
-
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("signup_div").style.display = "block";
-
-  }
-});
-
-function register(){
-
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  var userName = document.getElementById("contact_name").value;
-  var userNo = document.getElementById("contact_no").value;
-  var businessName = document.getElementById("business_name").value;
-  var licenseNo = document.getElementById("license_No").value
-
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-  /*firebase.database().ref('businessoperators/' + userId).set({
-    email: email_field,
-    userPass = password_field,
-    userName = contact_name,
-    userNo = contact_no,
-    businessName = business_name,
-    licenseNo = license_No
-  }); */
-    window.alert("Error : " + errorMessage);
-
-    // ...
-  });
-
-}
-
-/*function writeUserData(userEmail, userPass, userName, userNo, businessName, licenseNo) {
+function writeUserData(userEmail, userPass, userName, userNo, businessName, licenseNo) {
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
   var userName = document.getElementById("contact_name").value;
@@ -60,8 +14,5 @@ function register(){
     businessName = business_name,
     licenseNo = license_No
   });
-}*/
-
-function logout(){
-  firebase.auth().signOut();
 }
+
---
+++ js/viewjobs.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var rootRef = firebase.database().ref().child("Jobs");
+
+	rootRef.on("child_added", snap => {
+		var title = snap.child("jobTItle").val();
+		var companyName = snap.child("companyName").val();
+		var educationRequirement = snap.child("educationRequirement").val();
+		var category = snap.child("jobCategory").val();
+		var description = snap.child("jobDescription").val();
+		var poster = snap.child("jobPosterID").val();
+		var salary = snap.child("salary").val();
+		var userEmail = snap.child("userEmail").val();
+
+		$("#job-table").append("<tr><td>" + title + "</td><td>" + companyName + "</td><td>" + educationRequirement + "</td><td>" + category + "</td><td>" + description + "</td><td>" + poster + "</td><td>" + salary + "</td><td>" + userEmail + "</td>/tr>");
+		//$("#job-table").append("<tr><td>" + title + "</td><td>" + companyName +"</td><td> "+ educationRequirement"</td><td>" + category + "</td><td>" + description +"</td><td>"+ poster +"</td><td>" + salary + "</td><td>" + userEmail + "</td></tr>");
+	});
+
+});
