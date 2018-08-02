45cac67c17cab2bb4782b83a50c4fbe2153f7ca0
Sean <elitesean98@gmail.com>
2018-08-02 11:23:39 +0800

    Updating View User Function
    Edited Fireebase Variables

--- js/viewuser.js
+++ js/viewuser.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
 var rootRef = firebase.database().ref().child("User");
 
 rootRef.on("child_added", snap => {
-		var companyName = snap.child("companyName").val(); 
-		var companyDescription = snap.child("companyDesc").val();
-		var licenseNumber = snap.child("licenseNo").val();
-		var companyEmail = snap.child("companyEmail").val();
-		var personInCharge = snap.child("personInCharge").val();
-		var contactNumber = snap.child("contactNo").val(); 
+		var name = snap.child("name").val(); 
+		var profileDesc = snap.child("profileDesc").val();
+		var dateofBirth = snap.child("dateofBirth").val();
+		var email = snap.child("email").val();
+		var password = snap.child("password").val();
+		var qualification = snap.child("qualification").val(); 
 
 	//alert(snap.val()); /*Check to see if the site is getting anything*/
 
-		$("#user-table").append("<tr><td>" + companyName
-		+ "</td><td>" + companyDescription + "</td><td>" + 
-		licenseNumber + "</td><td>" + companyEmail + "</td><td>" + personInCharge 
-		+ "</td><td>" + contactNumber + "</td>/tr>"); 
+		$("#user-table").append("<tr><td>" + name
+		+ "</td><td>" + profileDesc + "</td><td>" + 
+		dateofBirth+ "</td><td>" + email + "</td><td>" + password 
+		+ "</td><td>" + qualification + "</td>/tr>"); 
 		
 });
 
